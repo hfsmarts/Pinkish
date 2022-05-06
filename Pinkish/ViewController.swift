fb6428c89dfd6ca26550d82ecbf9744fee6a5d40
Harun <harunfazlic@gmail.com>
2022-05-06 05:56:58 +0200

    Initial Commit

---
+++ Pinkish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pinkish
+//
+//  Created by Harun Fazlic on 6. 5. 2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
