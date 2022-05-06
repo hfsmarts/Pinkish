4ba8104ebafe16e59a9476f119869ebcfbac950e
Harun <harunfazlic@gmail.com>
2022-05-06 06:18:13 +0200

    Button title change

---
+++ Pinkish/TealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TealViewController.swift
+//  Pinkish
+//
+//  Created by Harun Fazlic on 6. 5. 2022..
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+title="Teal View"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
